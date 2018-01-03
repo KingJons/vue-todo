06f3b9f80bc2ece9cbd07fe4b096caef7ea89fdb
caicong <1161403069@qq.com>
2018-01-03 19:26:33 +0800

    入口文件

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// 入口文件
+import Vue from 'vue';
+// vue 构建webapp
+import App from './App.vue'; 
+// 将App挂载mount在页面root上（挂载点）
+
+new Vue({
+    el: '#root',
+    template: '<App />',
+    components: {
+        App
+    }
+})
