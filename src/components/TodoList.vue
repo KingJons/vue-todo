fcb312803cc8aafcb470e185aa9867ea6ec21cb6
caicong <1161403069@qq.com>
2018-01-06 21:13:31 +0800

    添加了todoitem组件

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>list</div>
+    <ul class="todo-list">
+        <todo-item v-for="(item, key) in collection" v-bind:key="key" :model="item"></todo-item>
+    </ul>
 </template>
 <script>
+import TodoItem from './TodoItem.vue'
 export default {
-  
+    props: ['collection'],
+    components: {
+        'todo-item': TodoItem
+    }
 }
 </script>
-<style>
-
+<style lang="stylus">
+.todo-list 
+    list-style none
 </style>
