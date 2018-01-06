ffe11bdffe5e636aab1e36db2088dc086be1742b
caicong <1161403069@qq.com>
2018-01-06 21:11:12 +0800

    添加了data

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,24 @@
             <todo-header></todo-header>
             <div class="todo-content">
                 <todo-report></todo-report>
-                <todo-list></todo-list>
+                <todo-list :collection="listState.items"></todo-list>
             </div>
         </div>
     </div>
 </template>
 <script>
+    import ListStore from '../data/store.js';
     import TodoHeader from './components/TodoHeader.vue';
     import TodoReport from './components/TodoReport.vue';
     import TodoList from './components/TodoList.vue';
     // 引用组件
 
     export default {
+        data(){
+            return {
+                listState: ListStore.state
+            }
+        },
         components: {
             TodoHeader,
             TodoReport,
@@ -24,6 +30,5 @@
     }
 </script>
 <style lang="stylus">
-    body
-        font-size 24px
+    @import "../styl/style.styl"
 </style>
