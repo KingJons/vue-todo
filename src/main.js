d6bf7cd37ca65ca85370fa71c13d2727f6cffaf6
caicong <1161403069@qq.com>
2018-01-06 21:15:07 +0800

    添加了vue-devtools

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 // vue 构建webapp
 import App from './App.vue'; 
 // 将App挂载mount在页面root上（挂载点）
-
+Vue.config.devtools = true;
 new Vue({
     el: '#root',
     template: '<App />',
