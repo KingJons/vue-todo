7af7360716ddb0f24e548423517540e23d58c3fe
caicong <1161403069@qq.com>
2018-01-06 21:12:53 +0800

    渲染了列表

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="todo-item">
+        <p class="text">{{model.text}}</p>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+    props: ['model']
+}
+</script>
